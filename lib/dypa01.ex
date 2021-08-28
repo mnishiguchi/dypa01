0f2bcbc067121d61ee8286085c5a40a11ee5a8ea
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-08-28 09:12:29 -0400

    Init

---
+++ lib/dypa01.ex
@@ -0,0 +1,18 @@
+defmodule DYPA01 do
+  @moduledoc """
+  Documentation for `DYPA01`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DYPA01.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dypa01_test.exs
@@ -0,0 +1,8 @@
+defmodule DYPA01Test do
+  use ExUnit.Case
+  doctest DYPA01
+
+  test "greets the world" do
+    assert DYPA01.hello() == :world
+  end
+end
